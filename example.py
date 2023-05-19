38acf9ebb6a9c27f6fe0ab7759db616ea42ad81e
qai <qai@mit.edu>
2023-05-19 15:58:58 -0400

    update example with log

--- example.py
+++ example.py
@@ -74,3 +74,31 @@ lab.act_by_instruction(ins_move_rack1_to_heater1)
 lab.act_by_instruction(ins_move_heater1_to_rack2)
 lab.act_by_instruction(ins_set_heater1_to_200)
 lab.act_by_instruction(ins_heater1_heating_to_set_point)
+"""
+2023-05-19 15:23:58.373 | WARNING  | hardware_pydantic.base:function_caller:26 - >> RUNNING *action__transfer* of *VialTransferor*: crane1
+2023-05-19 15:23:58.373 | INFO     | hardware_pydantic.base:function_caller:28 - action parameter name: from_obj
+2023-05-19 15:23:58.373 | INFO     | hardware_pydantic.base:function_caller:29 - action parameter value: identifier='rack1' content={'A1': 'vial1', 'A2': 'vial2', 'B1': 'vial3', 'B2': 'vial4'}
+2023-05-19 15:23:58.373 | INFO     | hardware_pydantic.base:function_caller:28 - action parameter name: to_obj
+2023-05-19 15:23:58.373 | INFO     | hardware_pydantic.base:function_caller:29 - action parameter value: identifier='heater1' set_point=25 reading=25 content=None
+2023-05-19 15:23:58.373 | INFO     | hardware_pydantic.base:function_caller:28 - action parameter name: transferee
+2023-05-19 15:23:58.373 | INFO     | hardware_pydantic.base:function_caller:29 - action parameter value: identifier='vial1' position='A1' position_relative='rack1' content={}
+2023-05-19 15:23:58.373 | INFO     | hardware_pydantic.base:function_caller:28 - action parameter name: to_position
+2023-05-19 15:23:58.373 | INFO     | hardware_pydantic.base:function_caller:29 - action parameter value: None
+2023-05-19 15:23:58.373 | WARNING  | hardware_pydantic.base:function_caller:31 - >> FINISHED with duration: 5
+2023-05-19 15:23:58.373 | WARNING  | hardware_pydantic.base:function_caller:26 - >> RUNNING *action__transfer* of *VialTransferor*: crane1
+2023-05-19 15:23:58.373 | INFO     | hardware_pydantic.base:function_caller:28 - action parameter name: from_obj
+2023-05-19 15:23:58.373 | INFO     | hardware_pydantic.base:function_caller:29 - action parameter value: identifier='heater1' set_point=25 reading=25 content='vial1'
+2023-05-19 15:23:58.373 | INFO     | hardware_pydantic.base:function_caller:28 - action parameter name: to_obj
+2023-05-19 15:23:58.373 | INFO     | hardware_pydantic.base:function_caller:29 - action parameter value: identifier='rack2' content={'A1': None, 'A2': None, 'B1': None, 'B2': None}
+2023-05-19 15:23:58.373 | INFO     | hardware_pydantic.base:function_caller:28 - action parameter name: transferee
+2023-05-19 15:23:58.373 | INFO     | hardware_pydantic.base:function_caller:29 - action parameter value: identifier='vial1' position=None position_relative='heater1' content={}
+2023-05-19 15:23:58.373 | INFO     | hardware_pydantic.base:function_caller:28 - action parameter name: to_position
+2023-05-19 15:23:58.373 | INFO     | hardware_pydantic.base:function_caller:29 - action parameter value: A2
+2023-05-19 15:23:58.373 | WARNING  | hardware_pydantic.base:function_caller:31 - >> FINISHED with duration: 5
+2023-05-19 15:23:58.373 | WARNING  | hardware_pydantic.base:function_caller:26 - >> RUNNING *action__set_point* of *Heater*: heater1
+2023-05-19 15:23:58.373 | INFO     | hardware_pydantic.base:function_caller:28 - action parameter name: set_point
+2023-05-19 15:23:58.373 | INFO     | hardware_pydantic.base:function_caller:29 - action parameter value: 200
+2023-05-19 15:23:58.373 | WARNING  | hardware_pydantic.base:function_caller:31 - >> FINISHED with duration: 1e-05
+2023-05-19 15:23:58.373 | WARNING  | hardware_pydantic.base:function_caller:26 - >> RUNNING *action__heat_process* of *Heater*: heater1
+2023-05-19 15:23:58.373 | WARNING  | hardware_pydantic.base:function_caller:31 - >> FINISHED with duration: 17.5
+"""
