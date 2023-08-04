1f3938c8b80fea2fb5d0b227692d9a1c7cc98b97
Fanwang Meng <fwmeng88@gmail.com>
2023-08-04 10:10:30 -0400

    Add documentation for `settings.py`

--- hardware_pydantic/tecan/settings.py
+++ hardware_pydantic/tecan/settings.py
@@ -13,18 +13,21 @@ TECAN_VIAL_TYPE = Literal["HRV", "MRV", "SV"]
 
 
 class TecanLabObject(LabObject):
-
+    """Base class for all Tecan lab objects."""
     def model_post_init(self, *args) -> None:
+        """Add the object to the Tecan lab."""
         TECAN_LAB.add_object(self)
 
 
 class TecanInstruction(Instruction):
-
+    """Base class for all Tecan instructions."""
     def model_post_init(self, *args) -> None:
+        """Add the instruction to the Tecan lab."""
         TECAN_LAB.add_instruction(self)
 
     @staticmethod
     def path_graph(ins_list: list[TecanInstruction]):
+        """Add preceding instructions to each instruction."""
         for i in range(1, len(ins_list)):
             ins = ins_list[i]
             pre_ins_id = ins_list[i-1].identifier
@@ -33,24 +36,55 @@ class TecanInstruction(Instruction):
 
 
 class TecanLayout(BaseModel):
-    """ a region appears in layout """
+    """A region appears in layout.
+
+    Parameters
+    ----------
+    layout_position : tuple[float, float] | None
+        left bot conor of the layout box. Default is None.
+    layout_x : float
+        The x length. Default is TECAN_LAYOUT_SLOT_SIZE_X.
+    layout_y : float
+        The y length. Default is TECAN_LAYOUT_SLOT_SIZE_Y.
+
+    """
 
     layout_position: tuple[float, float] | None = None
-    """ left bot conor of the layout box """
-
     layout_x: float = TECAN_LAYOUT_SLOT_SIZE_X
-    """ x length """
-
     layout_y: float = TECAN_LAYOUT_SLOT_SIZE_Y
-    """ y length """
 
     @classmethod
     def from_relative_layout(
             cls,
             layout_relation: Literal["above", "right_to"] = None,
             layout_relative: TecanLayout = None,
-            layout_x: float = TECAN_LAYOUT_SLOT_SIZE_X, layout_y: float = TECAN_LAYOUT_SLOT_SIZE_Y,
+            layout_x: float = TECAN_LAYOUT_SLOT_SIZE_X,
+            layout_y: float = TECAN_LAYOUT_SLOT_SIZE_Y,
     ):
+        """Create a TecanLayout from a relative layout.
+
+        Parameters
+        ----------
+        layout_relation : Literal["above", "right_to"], optional
+            The relation between the new layout and the relative layout. Default is None.
+        layout_relative : TecanLayout, optional
+            The relative layout. Default is None.
+        layout_x : float, optional
+            The x length. Default is TECAN_LAYOUT_SLOT_SIZE_X.
+        layout_y : float, optional
+            The y length. Default is TECAN_LAYOUT_SLOT_SIZE_Y.
+
+        Returns
+        -------
+        TecanLayout
+            The TecanLayout object.
+
+        Raises
+        ------
+        ValueError
+            If the layout_relation is not "above" or "right_to".
+
+        """
         if layout_relative is None:
             abs_layout_position = (0, 0)
         else:
