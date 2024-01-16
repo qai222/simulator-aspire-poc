e8d77b40db4c49c588e469d27a8387f86ea16fad
qai <qai@mit.edu>
2024-01-16 16:37:09 -0500

    better query

--- reaction_network/utils.py
+++ reaction_network/utils.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 import json
 
 import requests
@@ -6,16 +8,20 @@ from pandas._typing import FilePath
 ASKCOS_URL = "http://72.70.38.130"
 
 
-def query_askcos_condition_rec(reaction_smiles: str) -> dict:
-    response = requests.post(
+def query_askcos_condition_rec(reaction_smiles: str, return_query: bool=False) -> dict | tuple[dict, dict]:
+    q = dict(
         headers={"Content-type": "application/json"},
         url=f"{ASKCOS_URL}:9918/api/context-recommender/v2/predict/FP/call-sync",
         data=f'{{"smiles": "{reaction_smiles}", "n_conditions": 3}}',
     )
+    response = requests.post(**q)
     response = response.content.decode('utf8')
     response = json.loads(response)
     assert response['status_code'] == 200
-    return response
+    if return_query:
+        return response, q
+    else:
+        return response
 
 
 def drawing_url(smiles: str, size=80) -> str:
